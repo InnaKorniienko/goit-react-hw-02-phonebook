d185ee69627af9bdb17006d8456b61db80afbc1c
Inna <inna.kornieko@gmail.com>
2022-09-21 16:09:56 +0300

    Add components

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,41 +2,48 @@ import React, { Component } from "react";
 import PhonebookAdd from "./PhonebookAdd";
 import Contactlist from "./Contactlist";
 import { nanoid } from "nanoid";
+import Notiflix from "notiflix";
+import css from "./Phonebook.module.css";
 
 export default class Phonebook extends Component {
     state = {
-        contacts: [],
+        contacts: [
+            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
         filter: '',
       }
 
 addContact = (contact) => {
     if (this.isDublicate(contact)){
-        return alert(`${contact.name} - ${contact.number} is already on the site`)
+    return Notiflix.Notify.warning(`${contact.name} - ${contact.number} is already in contact`)
     }
     this.setState((prev) => {
-        const newContact = {
-            id: nanoid(),
-            ...contact
-        }
-        return {
-            contacts: [...prev.contacts, newContact]
-        }
+    const newContact = {
+    id: nanoid(),
+    ...contact
+    }
+    return {
+    contacts: [...prev.contacts, newContact]
+    }
     })
 }
 
 removeContact = (id) => {
     this.setState((prev) => {
-        const newContacts = prev.contacts.filter((item) => item.id !== id);
-        return {
-            contacts: newContacts
-        }
+    const newContacts = prev.contacts.filter((item) => item.id !== id);
+    return {
+    contacts: newContacts
+    }
     })
 }
 
 handleChange = (e) => {
     const { name, value } = e.target;
     this.setState({
-        [name]: value
+    [name]: value
     })
 }
 
@@ -49,15 +56,15 @@ isDublicate({name, number}) {
 getFilteredContacts() {
     const { contacts, filter } = this.state;
     if(!filter) {
-        return contacts;
-    }
+    return contacts;
+}
 
     const normalizedFilter = filter.toLocaleLowerCase();
     const filteredContacts = contacts.filter(({ name, number }) => {
-        const normalizedName = name.toLocaleLowerCase();
-        const normalizedNumber = number.toLocaleLowerCase();
-        const result = normalizedName.includes(normalizedFilter) || normalizedNumber.includes(normalizedFilter);
-        return result;
+    const normalizedName = name.toLocaleLowerCase();
+    const normalizedNumber = number.toLocaleLowerCase();
+    const result = normalizedName.includes(normalizedFilter) || normalizedNumber.includes(normalizedFilter);
+    return result;
     })
 
     return filteredContacts;
@@ -68,16 +75,22 @@ getFilteredContacts() {
         const { filter } = this.state;
         const contacts = this.getFilteredContacts();
         return (
-            <>
-            <div>
+            <div className={css.container}>
+            <div className={css.phonebook}>
+                <h2>Phoneboook</h2>
                 <PhonebookAdd onSubmit={addContact}/>
             </div>
-            <div>
-                <input type="text" name="filter" onChange={handleChange} value={filter}/>
+            <div className={css.contacts}>
+                <h2>Contacts</h2>
+                <div className={css.filter}>
+                <label className={css.label} > Find contacts by name </label>
+                <input className={css.input} type="text" name="filter" onChange={handleChange} value={filter}/>
+                </div>
                 <Contactlist items={contacts} removeContact={removeContact}/>
             </div>
-            
-            </>
+            </div>
         )
     }
 }
+
+
--- src/components/Phonebook/PhonebookAdd.jsx
+++ src/components/Phonebook/PhonebookAdd.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { nanoid } from "nanoid";
+import PropTypes from "prop-types";
+import css from "./Phonebook.module.css";
 
 export default class PhonebookAdd extends Component {
     state = {
@@ -34,10 +36,10 @@ handleSubmit = (e) => {
     render() {
         const { nameId, numberId, handleSubmit, handleChange } = this;
         return (
-            <form onSubmit={handleSubmit} >
-            <div>
-            <label htmlFor={nameId}> Name </label>
-            <input
+            <form className={css.form} onSubmit={handleSubmit} >
+            <div className={css.inputForm}>
+            <label className={css.label} htmlFor={nameId}> Name </label>
+            <input className={css.input}
                 id={nameId}
                 type="text"
                 name="name"
@@ -45,12 +47,13 @@ handleSubmit = (e) => {
                 onChange={handleChange}
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                placeholder="Please add name"
                 required
             />
             </div>
-            <div>
-            <label htmlFor={numberId}> Number </label>
-            <input
+            <div className={css.inputForm}>
+            <label className={css.label} htmlFor={numberId}> Number </label>
+            <input className={css.input}
                 id={numberId}
                 type="tel"
                 name="number"
@@ -58,12 +61,20 @@ handleSubmit = (e) => {
                 onChange={handleChange}
                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                placeholder="Please add telnumber"
                 required
             />
             </div>
-            <button> Add contact </button>
+            <button className={css.inputForm__button}> Add contact </button>
             </form>
         )
     }
 }
 
+
+PhonebookAdd.propTypes = {
+    stay: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+    })),
+}
